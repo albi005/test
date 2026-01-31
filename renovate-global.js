df505adc4bee0e7fa28be0a0a48332e0a816fb78
Albert Ragány-Németh <me@alb1.hu>
2026-01-31 23:26:52 +0100

    wip

--- renovate-global.js
+++ renovate-global.js
@@ -1,16 +1,30 @@
 module.exports = {
   platform: 'github',
-  // The repository we want to update
-  repositories: ['dummy-alb1/k8s-test'],
-  // Critical: This forces Renovate to use the Forking model
-  forkProcessing: "enabled",
-  // Optional: Optimize speed by only looking for docker updates
-  enabledManagers: ["kubernetes"], 
-  // enabledManagers: ["kubernetes", "kustomize"], 
-  // Optional: Clean up stale branches on the fork
-  prConcurrentLimit: 0,
-  prHourlyLimit: 0,
+  autodiscover: false,
   onboarding: false,
-  requireConfig: 'optional',
   forkToken: process.env.RENOVATE_TOKEN,
+  requireConfig: 'optional',
+  repositories: [
+    {
+      repository: 'dummy-alb1/k8s-test',
+      enabledManagers: ['kubernetes'],
+      kubernetes: {
+        fileMatch: ['startsch/startsch.yaml'],
+        // fileMatch: ['^startsch/startsch\\.yaml$'],
+      },
+      packageRules: [
+        {
+          description: 'Disable all updates by default for this repo',
+          matchPackagePatterns: ['*'],
+          enabled: false,
+        },
+        {
+          description: 'Enable only the specific image and force digest pinning',
+          matchPackageNames: ['ghcr.io/startsch/kir-dev'],
+          enabled: true,
+          pinDigests: true,
+        },
+      ],
+    },
+  ],
 };
