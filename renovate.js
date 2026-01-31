d8eae1fa44c65e3d463207efb78ff9070398c03d
Albert Ragány-Németh <me@alb1.hu>
2026-01-31 20:15:14 +0100

    ball

---
+++ renovate.js
@@ -0,0 +1,12 @@
+module.exports = {
+  platform: 'github',
+  // The repository we want to update
+  repositories: ['dummy-alb1/k8s-test'],
+  // Critical: This forces Renovate to use the Forking model
+  forkProcessing: "enabled",
+  // Optional: Optimize speed by only looking for docker updates
+  enabledManagers: ["kubernetes", "kustomize"], 
+  // Optional: Clean up stale branches on the fork
+  prConcurrentLimit: 0,
+  prHourlyLimit: 0,
+};
